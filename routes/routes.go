4301e4d30e47c7c0e742df836203d7eab1550f6b
Gnadlinger <gnadlinger.business@gmail.com>
2017-01-09 15:11:31 +0100

    forgot to commit

--- routes/routes.go
+++ routes/routes.go
@@ -10,13 +10,15 @@ func CreateRoutes(){
 	v1 := r.Group("api/v1")
 	{
 		v1.GET("/people", Handler.GetUsers)
+		v1.GET("/user/:username", Handler.GetUser)
 		v1.GET("/teams", Handler.GetTeams)
-		v1.GET("/members/:team", Handler.GetTeamMembers)
+		v1.POST("/addTeamToPlayer",Handler.AddTeamToPlayer)
+		/*v1.GET("/members/:team", Handler.GetTeamMembers)
 		v1.GET("/lineups/:team", Handler.GetLineUps)
-		/*v1.GET("/dates", handler.GetUseanrs)
-		v1.GET("/datetypes", handler.GetUsers)
-		v1.GET("/functiontypes", handler.GetUsers)
-		v1.GET("/functions", handler.GetUsers)
+		v1.GET("/participations/:username", Handler.GetParticipations)
+
+		v1.GET("/addteamtoplayer", Handler.AddTeamToPlayer)
+		/*v1.GET("/functions", handler.GetUsers)
 		v1.GET("/lineups", handler.GetUsers)
 		v1.GET("/squads", handler.GetUsers)
 		v1.GET("/teams", Handler.GetTeams)*/
